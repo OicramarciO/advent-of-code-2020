97f6763047fef457fcf3b7536b1fe3530108520f
Marcio <masmith@redventures.com>
2020-12-01 14:23:38 -0500

    solved day 1 part 2 and refactored

--- day-01/index.js
+++ day-01/index.js
@@ -1,12 +1,43 @@
 // Grab data from input.txt
 var fs = require("fs");
-var text = fs.readFileSync("./input.txt", "utf-8");
-var arrayOfNumbers = text.split("\n")
+var text = fs.readFileSync("./input2.txt", "utf-8");
+var arrInput = text.split("\n")
 
-for (var number in arrayOfNumbers) {
-  for(var secNumber in arrayOfNumbers){
-    if (parseInt(arrayOfNumbers[number]) + parseInt(arrayOfNumbers[secNumber]) == 2020){
-      console.log(arrayOfNumbers[number] * arrayOfNumbers[secNumber])
+function SumTwo(arrayOfNumbers, target){
+  for (var number in arrayOfNumbers) {
+    for(var secNumber in arrayOfNumbers){
+      if (
+        parseInt(arrayOfNumbers[number]) +
+        parseInt(arrayOfNumbers[secNumber])
+        == target)
+      {
+        console.log(arrayOfNumbers[number] * arrayOfNumbers[secNumber])
+        return
+      }
     }
   }
 }
+
+
+
+function SumThree(arrayOfNumbers, target){
+  for (var number in arrayOfNumbers) {
+    for(var secNumber in arrayOfNumbers){
+      for(var thirdNumber in arrayOfNumbers){
+        if (
+          parseInt(arrayOfNumbers[number]) +
+          parseInt(arrayOfNumbers[secNumber]) +
+          parseInt(arrayOfNumbers[thirdNumber]) 
+          == target)
+        {
+          console.log(arrayOfNumbers[number] * arrayOfNumbers[secNumber] * arrayOfNumbers[thirdNumber]);
+          return
+        }
+      }
+    }
+  }
+}
+//Part 1
+SumTwo(arrInput, 2020)
+//Part 2
+SumThree(arrInput, 2020)
